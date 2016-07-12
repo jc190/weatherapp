6e07a6926c79381b1630dc0470aa85eb7bb61e2e
James Calhoun <jim.calhoun@outlook.com>
2016-07-12 14:03:53 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
         + '<h3 id="location" class="location">' + this.weather.location + '</h3>'
         + '<p id="weather-date">' + this.weather.date + '</p>'
         + '<h1 class="temp"><span id="weather-temp">' + this.weather.temp + '</span>'
-        + '<span id="weather-deg">&degF</span></h1>'
+        + '<span id="weather-deg">&deg<a id="deg-type" href="#">F</a></span></h1>'
         + '<h4>Current Conditions:</h4><p id="weather-condition">' + this.weather.condition + '</p>'
         + '<h4>Wind:</h4><p id="weather-wind">' + this.weather.wind + '</p>'
         + '<h4>Precipitation:</h4><p id="weather-precip">' + this.weather.precip + '</p>'
@@ -24,23 +24,23 @@ $(document).ready(function() {
     weather: {
       setIcon: function(weatherCondition) {
         if (/cloud/gi.test(weatherCondition)) {
-          $('.bg-img').css('background-image', 'url("http://jtcstudio.com/images/cloudy.jpg")');
+          $('bg-img').css('backgound', 'url(\'http://jtcstudio.com/images/cloudy.jpg\')');
           return '<span class="typcn typcn-weather-cloudy"></span>';
         }
         if (/rain/gi.test(weatherCondition)) {
-          $('.bg-img').css('background-image', 'url("http://jtcstudio.com/images/rain.jpg")');
+          $('bg-img').css('backgound', 'url(\'http://jtcstudio.com/images/rain.jpg\')');
           return '<span class="typcn typcn-weather-shower"></span>';
         }
         if (/clear/gi.test(weatherCondition)) {
-          $('.bg-img').css('background-image', 'url("http://jtcstudio.com/images/sunny.jpg")');
+          $('bg-img').css('backgound', 'url(\'http://jtcstudio.com/images/sunny.jpg\')');
           return '<span class="typcn typcn-weather-sunny"></span>';
         }
         if (/storm/gi.test(weatherCondition)) {
-          $('.bg-img').css('background-image', 'url("http://jtcstudio.com/images/linghtning.jpg")');
+          $('bg-img').css('backgound', 'url(\'http://jtcstudio.com/images/lightning.jpg\')');
           return '<span class="typcn typcn-weather-stormy"></span>';
         }
         if (/snow/gi.test(weatherCondition)) {
-          $('.bg-img').css('background-image', 'url("http://jtcstudio.com/images/snow.jpg")');
+          $('bg-img').css('backgound', 'url(\'http://jtcstudio.com/images/snow.jpg\')');
           return '<span class="typcn typcn-weather-snow"></span>';
         }
       },
@@ -58,7 +58,6 @@ $(document).ready(function() {
       this.coords.long = Math.round(position.coords.longitude * 100) / 100;
     },
     updateWeather: function(data) {
-      console.log(data);
       this.weather.icon = this.weather.setIcon(data['current_observation']['weather']);
       this.weather.location = data['current_observation']['display_location']['full'];
       this.weather.date = data['current_observation']['observation_time_rfc822'];
@@ -67,11 +66,10 @@ $(document).ready(function() {
       this.weather.wind = data['current_observation']['wind_string'];
       this.weather.precip = data['current_observation']['precip_today_string'];
       this.weather.link = data['current_observation']['forecast_url'];
-      console.log(this.weather);
     },
     init: function() {
       if (!navigator.geolocation) {
-        this.domElement.html('<h1>Geolocation is not supported by your browser</h1>');
+        this.domElement.html('<h3 class="weather-error">Geolocation is not supported by your browser</h3>');
         return;
       }
       navigator.geolocation.getCurrentPosition(this.success.bind(this), this.error.bind(this));
@@ -82,18 +80,14 @@ $(document).ready(function() {
         url: this.setURL(),
         dataType: 'jsonp',
         success: function(data) {
-          console.log(data);
-          console.log(this.weather);
-          this.updateWeather(data);
-          console.log(this.weather);
-          this.domElement.html(this.html());
+          this.updateWeather();
+          this.domElement.html(this.html);
         }.bind(this)
       });
     },
     error: function() {
-      this.domElement.html('<h3>Oops something went wrong... Try again shortly.</h3>');
+      this.domElement.html('<h3 class="weather-error">Oops something went wrong... Try again shortly.</h3>');
     }
   };
-
   WeatherApp.init();
 });
