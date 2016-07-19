2c874a3ab8bc6e1554e6e7551be58829ea4d90b1
James Calhoun <jim.calhoun@outlook.com>
2016-07-19 15:07:24 -0400

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,11 +11,11 @@ $(document).ready(function() {
     },
     convertTemp: function(tempNum, tempType) {
       if (tempType === 'F') {
-        this.weather.temp = Math.round((((tempNum - 32) * (5/9)) * 100) / 100);
+        this.weather.temp = Math.round((((tempNum - 32) / 1.8) * 100)) / 100;
         this.weather.tempType = 'C';
       }
       if (tempType === 'C') {
-        this.weather.temp = Math.round((((tempNum + 32) * (9/5)) * 100) / 100);
+        this.weather.temp = Math.round(((tempNum * 1.8 + 32) * 100)) / 100;
         this.weather.tempType = 'F';
       }
       this.domElement.html(this.html());
